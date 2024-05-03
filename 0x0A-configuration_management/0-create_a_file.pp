7ac43c32164b3510a2b3e7781e9953b3e6d35805
fakeme-bew <nonvmem.2@gmail.com>
2024-05-04 02:49:14 +0300

    Task0: create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Creating a file named "school" with the specified path, permissions, owner, group, and content
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
