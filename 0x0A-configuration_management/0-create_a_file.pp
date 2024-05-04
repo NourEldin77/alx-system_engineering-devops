437b5971889fc3e388a3e4f02b28597997e8b74c
NourEldin77 <noureldean.2003@gmail.com>
2024-05-04 03:08:19 +0300

    Task0: create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Creating a file named "school" with the specified path, permissions, owner, group, and content
-file { 'school':
-  path    => '/tmp/school',
+# create a Puppet file
+
+file { '/tmp/school':
+  content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
 }
